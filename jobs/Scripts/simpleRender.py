b5cb6f2b6ffedac5f0dc6b37224854160bbff3bb
Andrey Kravchenko <epozine@inbox.ru>
2018-10-17 16:32:42 +0300

    log update

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -35,7 +35,8 @@ def main(args):
         scenes = f.read()
         scene_list = scenes.split(",\n")
 
-    cmdRun = ""
+    os.makedirs(os.path.join(args.output, "Color"))
+
     for each in scene_list:
 
         with open(os.path.join(args.res_path, args.package_name, each.split(".")[0] + ".json")) as f:
@@ -52,35 +53,36 @@ def main(args):
               #                                         res_path=args.res_path, resolution_x=args.resolution_x,
                #                                        resolution_y=args.resolution_y, package_name=args.package_name, scene_name=each)
 
-        ScriptPath = os.path.join(args.output, "cfg_" + each + ".json")
+        ScriptPath = os.path.join(args.output, "cfg_{}.json".format(each))
         with open(ScriptPath, 'w') as f:
             json.dump(jsonReport, f, indent=' ')
 
         scene = os.path.join(args.res_path, args.package_name, each)
-        cmdRun += '"{tool}" "{scene}" "{template}"\n'.format(tool=args.tool, scene=scene, template=ScriptPath)
+        cmdRun = '"{tool}" "{scene}" "{template}"\n'.format(tool=args.tool, scene=scene, template=ScriptPath)
 
-    cmdScriptPath = os.path.join(args.output, 'script.bat')
-    with open(cmdScriptPath, 'w') as f:
-        f.write(cmdRun)
+        cmdScriptPath = os.path.join(args.output, '{}.bat'.format(each))
+        with open(cmdScriptPath, 'w') as f:
+            f.write(cmdRun)
 
-    os.makedirs(os.path.join(args.output, "Color"))
-    os.chdir(args.output)
-    p = subprocess.Popen(cmdScriptPath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = p.communicate()
+        os.chdir(args.output)
+        p = subprocess.Popen(cmdScriptPath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
 
-    try:
-        rc = p.wait(timeout=200)
-    except psutil.TimeoutExpired as err:
-        rc = -1
-        for child in reversed(p.children(recursive=True)):
-            child.terminate()
-        p.terminate()
+        try:
+            rc = p.wait(timeout=200)
+        except psutil.TimeoutExpired as err:
+            rc = -1
+            for child in reversed(p.children(recursive=True)):
+                child.terminate()
+            p.terminate()
 
-    return rc
+        os.rename("tahoe.log", "{}.log".format(each)) 
+
+        
 
 
 if __name__ == "__main__":
 
     args = createArgsParser().parse_args()
-    rc = main(args)     
+    main(args)     
     exit(1)
