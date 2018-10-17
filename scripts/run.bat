f02a5c6bbe4f18eac2a5cae9c89369dc7034d9b6
Andrey Alekseevich <magister.demens@gmail.com>
2018-10-17 21:39:30 +0300

    added tests filter

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,3 +1,4 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
+set TESTS_FILTER="%1"
 
-python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Work\Results --work_dir Core --cmd_variables Tool "C:\rprSdkWin64\RprsRender64.exe" RenderDevice gpu ResPath "C:\TestResources\CoreAssets\scenes" PassLimit 50 rx 1000 ry 700
+python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Core --cmd_variables Tool "C:\rprSdkWin64\RprsRender64.exe" RenderDevice gpu ResPath "C:\TestResources\CoreAssets\scenes" PassLimit 50 rx 1000 ry 700
