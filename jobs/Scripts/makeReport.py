021b68a8caef9c05af1fd7f3c9d56bd887a7703d
Andrey Kravchenko <epozine@inbox.ru>
2018-10-17 15:00:35 +0300

    update makereport script

--- jobs/Scripts/makeReport.py
+++ jobs/Scripts/makeReport.py
@@ -8,82 +8,94 @@ def core_ver_str(core_ver):
     mn = (core_ver & 0xFFFFF) >> 8
     return "%x.%x" % (mj, mn)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--work_dir', required=True)
+def generateJsonForReport(directory):
 
-args = parser.parse_args()
-directory = args.work_dir
+    jsonForFormat = list(filter(lambda x: x.endswith('original.json'), os.listdir(directory)))
 
-files = os.listdir(directory)
-json_files = list(filter(lambda x: x.endswith('original.json'), files))
-result_json = ""
+    # format values 
+    for jsonReport in jsonForFormat:
 
-# format values 
-for file in json_files:
+        with open(os.path.join(directory, jsonReport)) as f:
+            tmp_json = f.read()
 
-    with open(os.path.join(directory, file)) as f:
-        tmp_json = f.read()
-    tmp_json = tmp_json.replace("\\", "\\\\")
-    testJson = json.loads(tmp_json)
-    report = {}
-    report["core_version"] = testJson["version"]
-    report["render_mode"] = "GPU"
-    report["render_device"] = testJson["gpu00"]
-    if "/" in testJson["input"]:
-        report["test_group"] = testJson["input"].split("/")[-2]
-        report["scene_name"] = testJson["input"].split("/")[-1]
-        report["test_case"] = testJson["input"].split("/")[-1]
-        report["file_name"] = testJson["input"].split("/")[-1] + ".png"
-        report["render_color_path"]  = os.path.join("Color", testJson["input"].split("/")[-1] + ".png")
-    else:
-        report["test_group"] = testJson["input"].split("\\")[-2]
-        report["scene_name"] = testJson["input"].split("\\")[-1]
-        report["test_case"] = testJson["input"].split("\\")[-1]
-        report["file_name"] = testJson["input"].split("\\")[-1] + ".png"
-        report["render_color_path"]  = os.path.join("Color", testJson["input"].split("\\")[-1] + ".png")
-    report["tool"] = "Core"
-    report["render_time"] = testJson["render.time.ms"] / 1000
-    report['date_time'] = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
-    report['difference_color'] = "not compared yet"
-    report['test_status'] = "passed"
-    report_name = file.replace("original", "RPR")
-    with open(os.path.join(directory, report_name), 'w') as f:
-        json.dump(report, f, indent=' ')
+        tmp_json = tmp_json.replace("\\", "\\\\")
+        testJson = json.loads(tmp_json)
+
+        report = {}
+        report["core_version"] = testJson["version"]
+        report["render_mode"] = "GPU"
+        report["render_device"] = testJson["gpu00"]
+        if "/" in testJson["input"]:
+            report["test_group"] = testJson["input"].split("/")[-2]
+            report["scene_name"] = testJson["input"].split("/")[-1]
+            report["test_case"] = testJson["input"].split("/")[-1]
+            report["file_name"] = testJson["input"].split("/")[-1] + ".png"
+            report["render_color_path"]  = os.path.join("Color", testJson["input"].split("/")[-1] + ".png")
+        else:
+            report["test_group"] = testJson["input"].split("\\")[-2]
+            report["scene_name"] = testJson["input"].split("\\")[-1]
+            report["test_case"] = testJson["input"].split("\\")[-1]
+            report["file_name"] = testJson["input"].split("\\")[-1] + ".png"
+            report["render_color_path"]  = os.path.join("Color", testJson["input"].split("\\")[-1] + ".png")
+        report["tool"] = "Core"
+        report["render_time"] = testJson["render.time.ms"] / 1000
+        report['date_time'] = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+        report['difference_color'] = "not compared yet"
+        report['test_status'] = "passed"
+
+        reportName = jsonReport.replace("original", "RPR")
+        with open(os.path.join(directory, reportName), 'w') as f:
+            json.dump([report], f, indent=' ')
 
 
-# collect all to one
-for file in range(len(json_files)):
+def generateReport(directory):
 
-    if (len(json_files) == 1):
-        f = open(os.path.join(directory, json_files[file]), 'r')
-        text = f.read()
-        f.close()
-        result_json += text
-        break
+    # collect all to one
+    files = os.listdir(directory)
+    json_files = list(filter(lambda x: x.endswith('RPR.json'), files))
 
-    if (file == 0):
-        f = open(os.path.join(directory, json_files[file]), 'r')
-        text = f.read()
-        f.close()
-        text = text[:-2]
-        text = text + "," + "\r\n"
-        result_json += text
+    result_json = ""
+    for file in range(len(json_files)):
 
-    elif (file == (len(json_files)) - 1):
-        f = open(os.path.join(directory, json_files[file]), 'r')
-        text = f.read()
-        f.close()
-        text = text[2:]
-        result_json += text
+        if (len(json_files) == 1):
+            f = open(os.path.join(directory, json_files[file]), 'r')
+            text = f.read()
+            f.close()
+            result_json += text
+            break
 
-    else:
-        f = open(os.path.join(directory, json_files[file]), 'r')
-        text = f.read()
-        f.close()
-        text = text[2:]
-        text = text[:-2]
-        text = text + "," + "\r\n"
-        result_json += text
+        if (file == 0):
+            f = open(os.path.join(directory, json_files[file]), 'r')
+            text = f.read()
+            f.close()
+            text = text[:-2]
+            text = text + "," + "\r\n"
+            result_json += text
 
-with open(os.path.join(directory, "report.json"), 'w') as file:
-    file.write(result_json)
+        elif (file == (len(json_files)) - 1):
+            f = open(os.path.join(directory, json_files[file]), 'r')
+            text = f.read()
+            f.close()
+            text = text[2:]
+            result_json += text
+
+        else:
+            f = open(os.path.join(directory, json_files[file]), 'r')
+            text = f.read()
+            f.close()
+            text = text[2:]
+            text = text[:-2]
+            text = text + "," + "\r\n"
+            result_json += text
+
+    with open(os.path.join(directory, "report.json"), 'w') as file:
+        file.write(result_json)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--work_dir', required=True)
+    args = parser.parse_args()
+
+    generateJsonForReport(args.work_dir)
+    generateReport(args.work_dir)
