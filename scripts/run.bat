5e5294a1ae77e2f9b6182438eae6e29aba4ab65a
Andrey Alekseevich <magister.demens@gmail.com>
2018-10-17 18:33:36 +0300

    changed tool path

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,3 +1,3 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 
-python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Work\Results --work_dir Core --cmd_variables Tool "C:\rprSdkWin64\lib\x64\RprsRender64.exe" RenderDevice gpu ResPath "C:\TestResources\CoreAssets\scenes" PassLimit 50 rx 1000 ry 700
+python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Work\Results --work_dir Core --cmd_variables Tool "C:\rprSdkWin64\RprsRender64.exe" RenderDevice gpu ResPath "C:\TestResources\CoreAssets\scenes" PassLimit 50 rx 1000 ry 700
