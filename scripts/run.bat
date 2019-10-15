757001e65c82a116d01c05bd3cdd53d70e99505a
Andrei Kravchenko <epozine@inbox.ru>
2019-10-15 23:59:29 +0300

    Revert "Engine param"
    This reverts commit 9a91cfe0f12580646840d16ec566e020dfb95b5b.

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,12 +1,13 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 set FILE_FILTER=%1
-set TESTS_FILTER="%2"
+set TESTS_FILTER=%2
 set RX=%3
 set RY=%4
 set PASS_LIMIT=%5
+set ENGINE=%6
 
 if "%RX%" EQU "" set RX=0
 if "%RY%" EQU "" set RY=0
 if "%PASS_LIMIT%" EQU "" set PASS_LIMIT=0
 
-python ..\jobs_launcher\executeTests.py --file_filter %FILE_FILTER% --test_filter %TESTS_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Core --cmd_variables Tool "C:\rprSdkWin64\RprsRender64.exe" RenderDevice gpu ResPath "C:\TestResources\CoreAssets" PassLimit %PASS_LIMIT% rx %RX% ry %RY%
+python ..\jobs_launcher\executeTests.py --file_filter %FILE_FILTER% --test_filter %TESTS_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Core --cmd_variables Tool "C:\rprSdkWin64\RprsRender64.exe" RenderDevice gpu ResPath "C:\TestResources\CoreAssets" PassLimit %PASS_LIMIT% rx %RX% ry %RY% engine %ENGINE%
