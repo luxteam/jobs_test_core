ad8aea88ac750988aabfae995092a608f181013b
Andrei Kravchenko <epozine@inbox.ru>
2019-10-21 15:10:46 +0300

    Update args parsing. Add additional logs for Hybrid AOV

--- scripts/run.bat
+++ scripts/run.bat
@@ -9,6 +9,5 @@ set ENGINE=%6
 if "%RX%" EQU "" set RX=0
 if "%RY%" EQU "" set RY=0
 if "%PASS_LIMIT%" EQU "" set PASS_LIMIT=0
-if "%ENGINE%" EQU "" set ENGINE=Tahoe64,Hybrid
 
 python ..\jobs_launcher\executeTests.py --file_filter %FILE_FILTER% --test_filter %TESTS_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Core --cmd_variables Tool "C:\rprSdkWin64\RprsRender64.exe" RenderDevice gpu ResPath "C:\TestResources\CoreAssets" PassLimit %PASS_LIMIT% rx %RX% ry %RY% engine_list %ENGINE%
