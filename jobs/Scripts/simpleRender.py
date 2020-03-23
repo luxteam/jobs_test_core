e1167320b415a66edbd69074e3c10d04ae4d6645
kataderon <kataderon@gmail.com>
2020-03-23 13:40:40 +0300

    remove expected json

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -60,8 +60,6 @@ def main():
 
         os.makedirs(os.path.join(args.output, "Color"))
         main_logger.info("Scenes to render: {}".format([name['scene'] for name['scene'] in scenes_list]))
-        with open(os.path.join(args.output, 'expected.json'), 'w') as file:
-            json.dump([s['scene'] for s in scenes_list], file, indent=4)
     except OSError as e:
         main_logger.error(str(e))
 
