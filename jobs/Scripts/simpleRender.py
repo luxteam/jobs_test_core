7bcbc434d13e5044c7ad6b8095efe80a338b6e9f
Ilia Nemankov <INemankov@luxoft.com>
2020-06-17 14:54:23 +0200

    Access os field in machine_info dict via get function

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -68,7 +68,7 @@ def main():
         exit(-1)
 
     gpu_name = get_gpu()
-    os_name = get_machine_info()['os']
+    os_name = get_machine_info().get('os', default='Unknown')
     if not gpu_name:
         main_logger.error("Can't get gpu name")
     if not os_name:
