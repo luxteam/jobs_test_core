541c3159f9102a55ce35472a29c1a42f756f8840
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-08-15 15:55:12 +0300

    remove old baselines structure support

--- scripts/make_results_baseline.sh
+++ scripts/make_results_baseline.sh
@@ -1,1 +1,1 @@
-python ../jobs_launcher/common/scripts/generate_baseline.py --results_root ../Work/Results/Core --baseline_root ../Work/Baseline
+python ../jobs_launcher/common/scripts/generate_baselines.py --results_root ../Work/Results/Core --baseline_root ../Work/Baseline
