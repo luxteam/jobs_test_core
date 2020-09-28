8efe6e291f8d620cc92fc46548fdfaee035e2c7a
Ilia Nemankov <INemankov@luxoft.com>
2020-09-28 19:06:42 +0200

    Add updateRefs param

--- scripts/run.bat
+++ scripts/run.bat
@@ -4,9 +4,11 @@ set TESTS_FILTER="%2"
 set RX=%3
 set RY=%4
 set PASS_LIMIT=%5
+set UPDATE_REFS=%6
 
 if "%RX%" EQU "" set RX=0
 if "%RY%" EQU "" set RY=0
 if "%PASS_LIMIT%" EQU "" set PASS_LIMIT=0
+if not defined UPDATE_REFS set UPDATE_REFS="No"
 
-python ..\jobs_launcher\executeTests.py --file_filter %FILE_FILTER% --test_filter %TESTS_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Core --cmd_variables Tool "..\\rprSdk\\RprsRender64.exe" RenderDevice gpu ResPath "C:\TestResources\CoreAssets" PassLimit %PASS_LIMIT% rx %RX% ry %RY%
+python ..\jobs_launcher\executeTests.py --file_filter %FILE_FILTER% --test_filter %TESTS_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Core --cmd_variables Tool "..\\rprSdk\\RprsRender64.exe" RenderDevice gpu ResPath "C:\TestResources\CoreAssets" PassLimit %PASS_LIMIT% rx %RX% ry %RY% UpdateRefs %UPDATE_REFS%
