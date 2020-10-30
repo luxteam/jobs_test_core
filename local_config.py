ec600971d3fe7b63d1798b46a22fdec5eb7be6ff
Ilia Nemankov <INemankov@luxoft.com>
2020-10-30 14:17:16 +0300

    Update local_config

--- local_config.py
+++ local_config.py
@@ -2,10 +2,6 @@ tool_name = 'core'
 report_type = 'ec'
 show_skipped_groups = True
 tracked_metrics = {
-    'gpu_memory_max': {'name': 'GPU memory max', 'function': 'avrg', 'displaying_unit': 'Mb'}, 
-    'gpu_memory_total': {'name': 'GPU memory total', 'function': 'avrg', 'displaying_unit': 'Mb'}, 
-    'gpu_memory_usage': {'name': 'GPU memory usage', 'function': 'avrg', 'displaying_unit': 'Mb'}, 
-    'system_memory_usage': {'name': 'System memory usage', 'function': 'avrg', 'displaying_unit': 'Mb'}, 
     'render_time': {'name': 'Render time', 'function': 'sum', 'displaying_unit': 's'}
 }
-tracked_metrics_files_number = 10
+tracked_metrics_files_number = 10000
