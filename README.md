f3840ee257c0802fafb6c4a24a2eb95eccaa3981
Artemy <kulbakowork@mail.ru>
2020-11-09 11:51:47 +0300

    Made reports parallel with render in simpleRender.py; Renamed SceneList.json to test_cases.json; Add field 'name' in test_cases.json; updated README.md to actual state; add requirements.txt check in run.bat and run.sh scripts

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-Core testing# jobs_test_core
+# Autotests for Radeon ProRender Core
+
+## Install
+ 1. Clone this repo
+ 2. Get `jobs_launcher` as git submodule, using next commands
+
+    ```
+    git submodule init
+    git submodule update
+    ```
+
+ 3. Put `CoreAssets` scenes placed in `C:/TestResources`.
+ 
+    ***You should use the specific scenes which defined in `test_cases.json` files in `jobs/Tests/` folders.***
+
+ 4. Run `run.bat` on Windows or `run.sh` only from `scripts` folder with customised arguments with space separator:
+
+    * Second arg sets `FILE_FILTER`.
+    * Third arg sets `TEST_FILTER`.
+    * Fourth arg sets `RX`. Default is `0`.
+    * Fifth arg sets `RY`. Default is `0`.
+    * Sixth arg sets `PASS_LIMIT`. Default is `0`.
+    * Seventh arg sets `UPDATE_REFS`. Default is `No`.
+
+    Example:
+    > run.bat Full.json
+
+    ***ATTENTION!***
+
+    **The order of the arguments is important. You cannot skip arguments.**
+
+    **Better to run via `CMD`. If you run through `PS`, empty arguments (like this "") will be ignored.**
--- jobs/Scripts/makeReport.py
+++ jobs/Scripts/makeReport.py
@@ -1,104 +1,12 @@
 import argparse
 import os
-import json
-import datetime
 import sys
 
 ROOT_DIR_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
 sys.path.append(ROOT_DIR_PATH)
 
-from jobs_launcher.core.system_info import get_gpu
-import jobs_launcher.core.config as core_config
 
-
-def core_ver_str(core_ver):
-    mj = (core_ver & 0xF00000) >> 20
-    mn = (core_ver & 0x00FF00) >> 8
-    r = (core_ver & 0x00000F)
-    return "%x.%x.%x" % (mj, mn, r)
-
-
-def generateJsonForReport(directory):
-
-    cfgJson = list(filter(lambda x: x.startswith('cfg_'), os.listdir(directory)))    
-    jsonForFormat = []
-    for i in cfgJson:
-        jsonForFormat.append("{}_original.json".format(i[4:-5]))
-
-    # FIXME: refactor report building of Core: make reports parallel with render
-    test_cases_path = os.path.join(directory, 'test_cases.json')
-    with open(test_cases_path, 'r') as cases_file:
-        cases = json.load(cases_file)
-    for case in cases:
-        case['status'] = 'done'
-
-    # format values 
-    for jsonReport in jsonForFormat:
-        if os.path.exists(os.path.join(directory,jsonReport)):
-            with open(os.path.join(directory, jsonReport)) as f:
-                tmp_json = f.read()
-
-            tmp_json = tmp_json.replace("\\", "\\\\")
-            testJson = json.loads(tmp_json)
-            reportName = jsonReport.replace("original", "RPR")
-
-            report = core_config.RENDER_REPORT_BASE.copy()
-            report.update(core_config.RENDER_REPORT_EC_PACK.copy())
-
-            report["core_version"] = core_ver_str(int(testJson["version"], 16))
-            report["minor_version"] = testJson["version.minor"]
-            report["gpu_memory_total"] = testJson["gpumem.total.mb"]
-            report["gpu_memory_max"] = testJson["gpumem.max.alloc.mb"]
-            report["gpu_memory_usage"] = testJson["gpumem.usage.mb"]
-            report["system_memory_usage"] = testJson["sysmem.usage.mb"]
-            report["render_mode"] = "GPU"
-            report["render_device"] = get_gpu()
-            report["test_group"] = directory.split(os.path.sep)[-1]
-            report["scene_name"] = testJson["input"].split(os.path.sep)[-1]
-            report["test_case"] = testJson["input"].split(os.path.sep)[-1]
-            if type(testJson["input"]) is str:
-                report["file_name"] = testJson["input"].split(os.path.sep)[-1] + ".png"
-            elif type(testJson["input"]) is list:
-                report["file_name"] = testJson["input"][0].split(os.path.sep)[-1] + ".png"
-            report["render_color_path"] = os.path.join("Color", testJson["input"].split(os.path.sep)[-1] + ".png")
-            report["tool"] = "Core"
-            report["render_time"] = testJson["render.time.ms"] / 1000
-            report['date_time'] = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-            report['difference_color'] = 0
-            report['test_status'] = "passed"
-            report['width'] = testJson['width']
-            report['height'] = testJson['height']
-            report['iterations'] = testJson['iteration']
-
-            with open(os.path.join(directory, reportName), 'r') as f:
-                rpr_report = json.load(f)[0]
-                report['tahoe_log'] = rpr_report['tahoe_log']
-                report['core_scene_configuration'] = rpr_report['core_scene_configuration']
-                report['group_timeout_exceeded'] = rpr_report['group_timeout_exceeded']
-            with open(os.path.join(directory, reportName), 'w') as f:
-                json.dump([report], f, indent=' ')
-
-            if 'aovs' in testJson.keys():
-                for key, value in testJson['aovs'].items():
-                    report["render_time"] = 0.0
-                    if type(value) is str:
-                        report['file_name'] = value.split(os.path.sep)[-1]
-                        report['render_color_path'] = value
-                    elif type(value) is list:
-                        report['file_name'] = value[0].split(os.path.sep)[-1]
-                        report['render_color_path'] = value[0]
-                    report['test_case'] = testJson['input'].split(os.path.sep)[-1] + key
-
-                    with open(os.path.join(directory, reportName.replace('_RPR', key + '_RPR')), 'r') as file:
-                        report['tahoe_log'] = json.load(file)[0]['tahoe_log']
-                    with open(os.path.join(directory, reportName.replace('_RPR', key + '_RPR')), 'w') as file:
-                        json.dump([report], file, indent=4)
-
-    with open(test_cases_path, 'w') as file:
-        json.dump(cases, file, indent=4)
-
-
-def generateReport(directory):
+def generate_report(directory):
 
     # collect all to one
     files = os.listdir(directory)
@@ -107,14 +15,14 @@ def generateReport(directory):
     result_json = ""
     for file in range(len(json_files)):
 
-        if (len(json_files) == 1):
+        if len(json_files) == 1:
             f = open(os.path.join(directory, json_files[file]), 'r')
             text = f.read()
             f.close()
             result_json += text
             break
 
-        if (file == 0):
+        if file == 0:
             f = open(os.path.join(directory, json_files[file]), 'r')
             text = f.read()
             f.close()
@@ -122,7 +30,7 @@ def generateReport(directory):
             text = text + "," + "\r\n"
             result_json += text
 
-        elif (file == (len(json_files)) - 1):
+        elif file == (len(json_files)) - 1:
             f = open(os.path.join(directory, json_files[file]), 'r')
             text = f.read()
             f.close()
@@ -143,10 +51,7 @@ def generateReport(directory):
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--work_dir', required=True)
     args = parser.parse_args()
-
-    generateJsonForReport(args.work_dir)
-    generateReport(args.work_dir)
+    generate_report(args.work_dir)
