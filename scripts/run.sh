86e2deca5b460f39793663c9036293e6903b8a85
Flame0n <36414392+Flame0n@users.noreply.github.com>
2021-03-09 02:01:53 +0300

    Changed python version 3.9

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,6 @@ RY=$4
 PASS_LIMIT=$5
 UPDATE_REFS=${6:-No}
 
-python -m pip install --user -r ../jobs_launcher/install/requirements.txt
+python3.9 -m pip install --user -r ../jobs_launcher/install/requirements.txt
 
-python ../jobs_launcher/executeTests.py --file_filter $FILE_FILTER --test_filter $TESTS_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Core --cmd_variables Tool "../rprSdk/RprsRender64" RenderDevice gpu ResPath "$CIS_TOOLS/../TestResources/rpr_core_autotests_assets" PassLimit $PASS_LIMIT rx $RX ry $RY UpdateRefs $UPDATE_REFS
+python3.9 ../jobs_launcher/executeTests.py --file_filter $FILE_FILTER --test_filter $TESTS_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Core --cmd_variables Tool "../rprSdk/RprsRender64" RenderDevice gpu ResPath "$CIS_TOOLS/../TestResources/rpr_core_autotests_assets" PassLimit $PASS_LIMIT rx $RX ry $RY UpdateRefs $UPDATE_REFS
